d83a0d6d170fc5239aa5fc2e4be99dd28bd0a343
AndreOGodoy <godoy.andre@hotmail.com>
2020-04-08 15:04:02 -0300

    Adding dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate graphics;
+extern crate sprite;
+extern crate piston;
+extern crate opengl_graphics;
+extern crate glutin_window;
+
 fn main() {
-    println!("Hello, world!");
+    
 }

1a62f140ad5e9ec3d58d5694a6981d4c1ea9d76d
AndreOGodoy <godoy.andre@hotmail.com>
2020-04-08 15:26:47 -0300

    Basic main loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,37 @@
-extern crate graphics;
-extern crate sprite;
-extern crate piston;
-extern crate opengl_graphics;
 extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+extern crate sprite;
+
+use glutin_window::GlutinWindow;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events, EventLoop};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, ButtonArgs, ButtonEvent};
+use piston::window::WindowSettings;
 
 fn main() {
-    
+    let opengl = OpenGL::V4_5;
+
+    let mut window: GlutinWindow = WindowSettings::new("Snake Game", [800, 600])
+        .exit_on_esc(true)
+        .build()
+        .expect("Coudn't open window");
+
+    let mut event = Events::new(EventSettings::new());
+
+    while let Some(e) = event.next(&mut window) {
+        if let Some(arg) = e.render_args() {
+            //Do something here
+        }
+
+        if let Some(arg) = e.update_args() {
+            //Do something here
+        }
+
+        if let Some(arg) = e.button_args() {
+            //Do something here
+        }
+    }
+
 }
